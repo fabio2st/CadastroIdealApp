7ed8201338bffc07772b8066a1276fec63499b43
Fabio Dos Santos <fabio2st@yahoo.com>
2021-02-11 18:16:24 -0300

    Delete pessoa

--- CadastroIdealApp/PessoasCadastradasCrud.xaml.vb
+++ CadastroIdealApp/PessoasCadastradasCrud.xaml.vb
@@ -19,21 +19,40 @@ Partial Class PessoasCadastradasCrud
 
     End Sub
     Async Sub PessoasCadastradasCrud_Loaded(sender As Object, e As RoutedEventArgs)
+        Await PessoaSelectAll()
+    End Sub
+    Private Sub EditarButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim pessoaCadastradaEdit As New PessoaCadastradaEdit()
+        NavigationService.Navigate(pessoaCadastradaEdit)
+    End Sub
+    Private Async Sub RemoverButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim pessoa As Pessoa = pessoasCadastradasListBox.SelectedItem
+        If pessoa IsNot Nothing Then
+            If MessageBox.Show("Tem certeza que está removendo " & pessoa.ToString(), Me.Title, MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) Then
+                Await PessoaDelete(pessoa)
+            End If
+        End If
+    End Sub
+    Private Async Function PessoaSelectAll() As Task
         Try
             Dim response = Await client.GetAsync("/api/Pessoas")
             response.EnsureSuccessStatusCode()
             Dim pessoas = Await response.Content.ReadAsAsync(Of IEnumerable(Of Pessoa))()
-            Console.WriteLine(pessoas)
+            pessoasCadastradasListBox.Items.Clear()
             For Each pessoa As Pessoa In pessoas
                 pessoasCadastradasListBox.Items.Add(pessoa)
             Next
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
-    End Sub
-
-    Private Sub ButtonClick(sender As Object, e As RoutedEventArgs)
-        Dim pessoaCadastradaEdit As New PessoaCadastradaEdit()
-        NavigationService.Navigate(pessoaCadastradaEdit)
-    End Sub
+    End Function
+    Private Async Function PessoaDelete(pessoa As Pessoa) As Task
+        Try
+            Dim response = Await client.DeleteAsync($"/api/Pessoas/{pessoa.ID}")
+            Await PessoaSelectAll()
+            'Return response.StatusCode
+        Catch ex As Exception
+            MessageBox.Show($"A operação falhou: {ex.Message}")
+        End Try
+    End Function
 End Class
